0b5a47d6b3912d821acb551631b92f56636724ae
Łukasz Godziejewski <lgodziejewski@gmail.com>
2018-12-03 22:52:42 +0100

    Implement day-1 second task

--- day-1/main.js
+++ day-1/main.js
@@ -1,13 +1,44 @@
 const fs = require('fs');
+const path = require('path');
 const readline = require('readline');
 
-const reader = readline.createInterface(fs.createReadStream('./input'));
+const inputPath = path.format({ dir: __dirname, base: 'input' });
+const reader = readline.createInterface(fs.createReadStream(inputPath));
 
 const lines = [];
 reader.on('line', line => lines.push(+line));
 
 reader.on('close', () => {
-    const result = lines.reduce((res, value) => res += value, 0);
+    const firstResult = calculateFirstTask(lines);
+    console.log('first result: ', firstResult);
 
-    console.log('result: ', result);
+    const secondResult = calculateSecondTask(lines);
+    console.log('second result: ', secondResult);
 })
+
+function calculateFirstTask(lines) {
+    return lines.reduce((res, line) => res += line);
+}
+
+function calculateSecondTask(lines) {
+    let currentFreq = 0;
+    const frequenciesSet = new Set();
+    frequenciesSet.add(currentFreq);
+    let found = false;
+
+    let repeat = 0;
+    while(!found) {
+        for (line of lines) {
+            currentFreq = currentFreq + line;
+
+            if (frequenciesSet.has(currentFreq)) {
+                found = true;
+                break;
+            } else {
+                frequenciesSet.add(currentFreq);
+            }
+        }
+    }
+
+    return currentFreq;
+}
