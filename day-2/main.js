b7fd616ece5ef4b41c5d609c49d32e63a056e35a
Łukasz Godziejewski <lgodziejewski@gmail.com>
2018-12-03 23:28:16 +0100

    Implement day-2 task one

--- day-2/main.js
+++ day-2/main.js
@@ -2,7 +2,6 @@ const { fileToArray } = require('../fileToArray');
 const dir = __dirname;
 
 fileToArray(dir, 'input').then(input => {
-    console.log('input: ', input);
     const firstResult = calculateFirstTask(input);
     console.log('first result: ', firstResult);
 
@@ -11,9 +10,41 @@ fileToArray(dir, 'input').then(input => {
 });
 
 function calculateFirstTask(input) {
+    let twoSameLetterIDCount = 0;
+    let threeSameLetterIDCount = 0;
 
+    input.forEach(word => {
+        const letters = countLetters(word);
+        
+        const results = hasTwoOrThreeSameLetters(letters);
+        if (results.two) twoSameLetterIDCount++;
+        if (results.three) threeSameLetterIDCount++;
+    });
+
+    return twoSameLetterIDCount * threeSameLetterIDCount;
 }
 
 function calculateSecondTask(input) {
 
+}
+
+function countLetters(word) {
+    const result = {};
+    word.split('').forEach(letter => {
+        if (result[letter]) result[letter]++;
+        else result[letter] = 1;
+    });
+
+    return result;
+}
+
+function hasTwoOrThreeSameLetters(letters) {
+    const res = { two: false, three: false };
+
+    Object.keys(letters).forEach(letter => {
+        if (letters[letter] === 2) res.two = true;
+        if (letters[letter] === 3) res.three = true;
+    });
+
+    return res;
 }
