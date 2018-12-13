6cfa81b02b5595c5baf545bb091f1510bb69efba
Łukasz Godziejewski <lgodziejewski@gmail.com>
2018-12-13 22:14:05 +0100

    Implement day-13 task 1

---
+++ day-13/main.js
@@ -0,0 +1,285 @@
+const { fileToArray } = require('../fileToArray');
+const dir = __dirname;
+
+const myData = {
+    file: 'input',
+};
+
+const testData = {
+    file: 'test',
+}
+
+const usedData = myData;
+
+const fieldType = {
+    vertical: 0,
+    horizontal: 1,
+    curveLeft: 2,
+    curveRight: 3,
+    cross: 4,
+    empty: 5,
+};
+
+const signs = {
+    [fieldType.vertical]: '|',
+    [fieldType.horizontal]: '-',
+    [fieldType.curveLeft]: '/',
+    [fieldType.curveRight]: '\\',
+    [fieldType.cross]: '+',
+    [fieldType.empty]: ' ',
+};
+
+const cartSigns = {
+    up: '^',
+    right: '>',
+    down: 'v',
+    left: '<',
+};
+
+const turnMap = {
+    left: {
+        up: 'left',
+        right: 'up',
+        down: 'right',
+        left: 'down',
+    },
+    straight: {
+        up: 'up',
+        right: 'right',
+        down: 'down',
+        left: 'left',
+    },
+    right: {
+        up: 'right',
+        right: 'down',
+        down: 'left',
+        left: 'up',
+    },
+};
+
+const nextTurn = {
+    left: 'straight',
+    straight: 'right',
+    right: 'left',
+};
+
+const rightCurveMap = {
+    up: 'left',
+    right: 'down',
+    left: 'up',
+    down: 'right',
+};
+
+const leftCurveMap = {
+    up: 'right',
+    right: 'up',
+    down: 'left',
+    left: 'down',
+};
+
+fileToArray(dir, usedData.file).then(input => {
+    const parsedInput = parseInput(input);
+
+    const firstResult = calculateFirstTask(parsedInput);
+    console.log('first result: ', firstResult);
+
+    const secondResult = calculateSecondTask(parsedInput);
+    console.log('second result: ', secondResult);
+});
+
+function parseInput(input) {
+    const result = [];
+    const carts = [];
+    let rowId = 0;
+    for (row of input) {
+        const parsedRow = [];
+        const chars = row.split('');
+        let colId = 0;
+        for (char of chars) {
+            const position = { x: colId, y: rowId };
+            const up = { x: colId, y: rowId - 1 };
+            const right = { x: colId + 1, y: rowId };
+            const down = { x: colId, y: rowId + 1 };
+            const left = { x: colId - 1, y: rowId };
+
+            let road = { position, type: fieldType.empty };
+
+            if (char === '|' || char === '^' || char === 'v') {
+                road = {
+                    type: fieldType.vertical,
+                    position,
+                    next: { up, down },
+                };
+            } else if (char === '-' || char === '<' || char === '>') {
+                road = {
+                    type: fieldType.horizontal,
+                    position,
+                    next: { left, right },
+                };
+            } else if (char === '+') {
+                road = {
+                    type: fieldType.cross,
+                    position,
+                    next: { up, right, down, left },
+                };
+            } else if (char === '/') {
+                // bottom-right
+                if (chars[colId - 1] === '-') {
+                    road = {
+                        type: fieldType.curveLeft,
+                        position,
+                        next: { left, up },
+                    };
+                // top-left
+                } else {
+                    road = {
+                        type: fieldType.curveLeft,
+                        position,
+                        next: { right, down },
+                    };
+                }
+            } else if (char === '\\') {
+                // top-right
+                if (chars[colId - 1] === '-') {
+                    road = {
+                        type: fieldType.curveRight,
+                        position,
+                        next: { left, down },
+                    };
+                // bottom-left
+                } else {
+                    road = {
+                        type: fieldType.curveRight,
+                        position,
+                        next: { right, up },
+                    };
+                }
+            }
+
+            let cart;
+            const nextTurn = 'left';
+            // carts
+            switch (char) {
+                case '<':
+                    cart = { position, orientation: 'left', nextTurn };
+                    break;
+                case '^':
+                    cart = { position, orientation: 'up', nextTurn };
+                    break;
+                case '>':
+                    cart = { position, orientation: 'right', nextTurn };
+                    break;
+                case 'v':
+                    cart = { position, orientation: 'down', nextTurn };
+                    break;
+                default:
+                    // do nothing
+            }
+
+            if (cart) carts.push(cart);
+            if (road) parsedRow.push(road);
+            colId++;
+        }
+
+        result.push(parsedRow);
+        rowId++;
+    }
+
+    return { map: result, carts };
+}
+
+function printMap({ map, carts }, crashCoords) {
+    const result = [];
+
+    for (row of map) {
+        const resultRow = [];
+        for (el of row) {
+            resultRow.push(signs[el.type]);
+        }
+        result.push(resultRow);
+    }
+
+    for (cart of carts) {
+        result[cart.position.y][cart.position.x] = cartSigns[cart.orientation];
+    }
+
+    if (crashCoords) result[crashCoords.y][crashCoords.x] = 'X';
+
+    const printedRows = result.map(row => row.join(''));
+    console.log(printedRows.join('\n'));
+}
+
+function calculateFirstTask(data) {
+
+    const { map, carts: oldCarts } = data;
+    const carts = JSON.parse(JSON.stringify(oldCarts));
+    let crash = false;
+    let iter = 0;
+    while (!crash) {
+        // sort carts on each iteration, so that they move y first, x second
+        carts.sort((a, b) => {
+            if (a.position.y === b.position.y) return a.position.x - b.position.x;
+
+            return a.position.y - b.position.y;
+        });
+
+        for (cart of carts) {
+            const nextPosition = getNextPosition(cart.position, cart.orientation);
+            
+            // check crash
+            const isCrash = carts.some(cart => cart.position.x === nextPosition.x && cart.position.y === nextPosition.y);
+            if (isCrash) {
+                crash = nextPosition;
+            }
+            
+            const nextField = map[nextPosition.y][nextPosition.x];
+
+            // cart.nextTurn changed inside
+            const nextOrientation = getNextOrientation(cart, nextField)
+            cart.position = nextPosition,
+            cart.orientation = nextOrientation;
+        }
+
+        iter++;
+    }
+
+    // console.log('crash!');
+    // printMap(data, crash);
+
+    return crash;
+}
+
+function calculateSecondTask(data) {
+
+}
+
+function getNextPosition(position, orientation) {
+    const diffs = {
+        up: { x: 0, y: -1 },
+        right: { x: 1, y: 0 },
+        down: { x: 0, y: 1 },
+        left: { x: -1, y: 0 },
+    };
+    const diff = diffs[orientation];
+
+    return {
+        x: position.x + diff.x,
+        y: position.y + diff.y,
+    };
+}
+
+function getNextOrientation(cart, field) {
+    let nextOrientation;
+    if (field.type === fieldType.vertical || field.type === fieldType.horizontal) {
+        nextOrientation = cart.orientation;
+    } else if (field.type === fieldType.curveRight) {
+        nextOrientation = rightCurveMap[cart.orientation];
+    } else if (field.type === fieldType.curveLeft) {
+        nextOrientation = leftCurveMap[cart.orientation];
+    } else if (field.type === fieldType.cross) {
+        nextOrientation = turnMap[cart.nextTurn][cart.orientation];
+        cart.nextTurn = nextTurn[cart.nextTurn];
+    }
+
+    return nextOrientation;
+}
